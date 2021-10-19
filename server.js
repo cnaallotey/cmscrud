52072e2e509d56cc2892789e1b2d1806685b5059
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2021-10-19 10:40:47 +0000

    heroku

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/', serveStatic(path.join(__dirname, 'dist')))
 
 
 
-const port = process.env.PORT || 39000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> {
     console.log(`App is running on ${port}`);
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -200,6 +200,7 @@
               </table>
             </div>
           </div>
+          
           <modal
             v-if="showmodal"
             :addClient="addClient"
@@ -258,6 +259,7 @@ export default {
       // year: "",
       // car: "",
       // cars: ["toyota", "hyundai", "mazda"],
+     
       showmodal: false,
       editmodal: false,
       employees: [],
@@ -291,7 +293,7 @@ export default {
         this.error = true;
       } else {
         axios
-          .post("http://localhost:39000/client", {
+          .post("http://localhost:3000/client", {
             Name: { firstName: this.firstName, lastName: this.lastName },
             department: this.department,
             email: this.email,
@@ -328,7 +330,7 @@ export default {
 
     editClient: function (clientId) {
       axios
-        .get("http://localhost:39000/client/editclient/" + clientId)
+        .get("http://localhost:3000/client/editclient/" + clientId)
         .then((response) => {
           console.log(response.data);
           const update = response.data;
@@ -346,7 +348,7 @@ export default {
       // this.employees = this.employees.filter((item) => item.id !== employee.id);
     },
     updateClient: function () {
-      axios.put("http://localhost:39000/client/updateclient/" + this.id, {
+      axios.put("http://localhost:3000/client/updateclient/" + this.id, {
         firstName: this.firstName,
         lastName: this.lastName,
         role: this.role,
@@ -360,7 +362,7 @@ export default {
     },
     toggleEditModal: function () {},
     deleteClient: function (clientId) {
-      axios.delete("http://localhost:39000/client/" + clientId).then((response) => {
+      axios.delete("http://localhost:3000/client/" + clientId).then((response) => {
         console.log(response.data);
         location.reload();
       });
@@ -386,7 +388,7 @@ export default {
   mounted() {
     axios
       //load all employees
-      .get("http://localhost:39000/loadclient")
+      .get("http://localhost:3000/loadclient")
       .then((res) => (this.employees = res.data))
       .catch((err) => console.log(err));
   },
